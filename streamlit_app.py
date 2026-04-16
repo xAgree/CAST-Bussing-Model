ca3233a01c47270f23f67c6e78f8d591f2cece26
xAgree <contact.ethanyoung@gmail.com>
2026-04-16 14:05:18 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ transit_time = 21.7
 FLIGHT_LOAD_FACTOR = .86
 
 
-Arrival_Rollover = pd.Timedelta(minutes=Arrival_TimeFrame-15)
+Arrival_Rollover = pd.Timedelta(minutes=Arrival_TimeFrame)
 Departure_Rollover = pd.Timedelta(minutes=Departure_TimeFrame)
 Domestic_Rollover = pd.Timedelta(minutes=Domestic_TimeFrame)
 
